c3e240afd7a36ba78853650e2169690391a39d83
Daniel Marko <20088708@mail.wit.ie>
2022-12-07 01:25:38 +0000

    Mongo lab exercises done.

--- movies-api/api/genres/index.js
+++ movies-api/api/genres/index.js
@@ -1,11 +1,12 @@
 import express from 'express';
-import { genres } from './genreData';
+import Genre from './genreModel';
 
 
 const router = express.Router(); 
 
-router.get('/', (req, res) => {
-    res.json(genres);
+router.get('/', async (req, res) => {
+    const genres = await Genre.find();
+    res.status(200).json(genres);
 });
 
 export default router;
